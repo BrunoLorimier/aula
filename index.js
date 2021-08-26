9326bc67267b7d3186887bc49322db6bdac8b02e
Bruno <brunolorimier@hotmail.com>
2021-08-25 22:08:03 -0300

    Aula dia 24/08

--- index.js
+++ index.js
@@ -1,9 +1,135 @@
-const triplo = function (n = 5){
-    return n * 3
-}
+const fs = require("fs");
+const abrirArquivo = function (nomeArquivo){
+    const exibirConteudo = function (erro, conteudo){
+        if (erro){
+            console.log(`Deu erro: ${erro}`);
+        } else{
+            console.log(conteudo.toString());
+        }
+    };
+    fs.readFile(nomeArquivo, exibirConteudo);
+};
+abrirArquivo("arquivo.txt");
 
-console.log(triplo(8))
-console.log(triplo())
+// function demorada(tempo){
+//     console.log(`demorada ${tempo}`);
+//     const atualMaisTempo = new Date().getTime() + tempo;
+//     while(new Date().getTime() <= atualMaisTempo);
+//     const d = 8 + 4;
+//     return d;
+// }
+// setTimeout(function(){demorada(2000)}, 2000);
+// setTimeout(function(){demorada(1000)}, 1000);
+// console.log("chegou ao fim do script principal");
+
+// setTimeout(function(){
+//     console.log('dentro da timeout', 0)
+// })
+// const a = new Date().getTime() + 1000
+// while (new Date().getTime() <= a);
+// console.log('fora da timeout')
+
+// function demorada(){
+//     const atualMais2Segundos = new Date().getTime() + 2000
+//     while (new Date().getTime <= atualMais2Segundos);
+//     const d = 8+4
+//     return d
+// }
+// const a = 2+3
+// const b = 5+9
+
+// setTimeout(function(){
+//     const d = demorada()
+//     console.log(d)
+// }, 500)
+
+// const e = 2 + a + b
+// console.log(e)
+
+// let calculadora = {
+//     //arrow function
+//     soma: (a, b) => a + b,
+//     //função comum
+//     subtracao: function(a, b){
+//         return a - b;
+//     },
+// };
+
+// console.log(`2 + 3 = ${calculadora.soma(2, 3)}`);
+// console.log(`2 - 3 = ${calculadora.subtracao(2, 3)}`);
+
+// let concessionaria = {
+//     cnpj: "00011122233344401-45",
+//     endereco: {
+//         logradouro: "Rua A",
+//         numero: 10,
+//         bairro: "Vila J",
+//     },
+//     veiculos: [
+//         {
+//             marca: "Ford",
+//             modelo: "Ecosport",
+//             anoDeFabricacao: 2018,
+//         },
+//         {
+//             marca: "Chevrolet",
+//             modelo: "Onix",
+//             anoDeFabricacao: 2020,
+//         },
+//         {
+//             marca: "Volkswagen",
+//             modelo:"Nivus",
+//             anoDeFabricacao: 2020,
+//         },
+//     ],
+// };
+// for (let veiculo of concessionaria.veiculos){
+//     console.log(`Marca: ${veiculo.marca}`);
+//     console.log(`Modelo: ${veiculo.modelo}`);
+//     console.log(`Ano de Fabricação: ${veiculo.anoDeFabricacao}`);
+// }
+
+// let pessoaComEndereco = {
+//     nome: "Maria",
+//     idade: 21,
+//     endereco: {
+//         logradouro: "Rua B",
+//         numero: 121,
+//     },
+// };
+
+// console.log(
+//     `Sou ${pessoaComEndereco.nome},
+//     tenho ${pessoaComEndereco.idade} anos
+//     e moro na ${pessoaComEndereco.endereco["logradouro"]}
+//     número ${pessoaComEndereco["endereco"]["numero"]}`
+// );
+
+// let pessoa = {
+//     nome: "João",
+//     idade: 17,
+// }
+
+// console.log("Me chamo " + pessoa.nome);
+
+// console.log("Tenho " + pessoa["idade"] + " anos");
+
+// function saudacoesFactory(saudacao, nome){
+//     return function (){
+//         console.log (saudacao + ', ' + nome);
+//     }
+// }
+// let olaJoao = saudacoesFactory ('Olá', 'João');
+// let tchauJoao = saudacoesFactory('Tchau', 'João');
+// olaJoao();
+// tchauJoao();
+
+// const triplo = function (n = 5){
+//     return n * 3
+// }
+
+// console.log(triplo(8))
+// console.log(triplo())
 
 //const triplo = function (n){
     //if(n !== undefined){
